76ea870f08a54d8be0ee79902107bb1abb8a41de
PauloVitor <vitorldomingues@gmail.com>
2025-05-13 11:48:45 -0300

    Adicionei dados especificos para API buscar

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 if (isset($_GET['pokemon'])){
     $nomePokemon = ($_GET['pokemon']);
     $url = "https://pokeapi.co/api/v2/pokemon/{$nomePokemon}";
+
     $resposta = file_get_contents($url);
    
     if ($resposta){ 
@@ -16,10 +17,15 @@ if (isset($_GET['pokemon'])){
     $nome = ucfirst($dados['name']);
     $id = $dados['id'];
     $imagem = $dados['sprites']['front_default'];
-    
+    $tipos = array_map(fn($t) => $t['type']['name'], $dados['types']);
+    $altura = $dados['height'] / 10; // metros
+    $peso = $dados['weight'] / 10;  // kg
 
     echo "<h2>$nome (ID: $id)</h2>";
     echo "<img src='$imagem' alt='$nome'><br>";
+    echo "<strong>Tipos:</strong> " . implode(', ', $tipos) . "<br>";
+    echo "<strong>Altura:</strong> {$altura} m<br>";
+    echo "<strong>Peso:</strong> {$peso} kg<br>";
 } else {
     echo "Pokemon nao encontrado";
     }
