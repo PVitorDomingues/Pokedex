aa7d9a1ebae5b128c0be5adff3a74075a6d22a63
PauloVitor <vitorldomingues@gmail.com>
2025-05-13 11:12:35 -0300

    Adicionei integração com API do Pokemon

--- index.php
+++ index.php
@@ -3,4 +3,25 @@
     <button type="submit">Buscar</button>
 </form>
 
+<?php
 
+if (isset($_GET['pokemon'])){
+    $nomePokemon = ($_GET['pokemon']);
+    $url = "https://pokeapi.co/api/v2/pokemon/{$nomePokemon}";
+    $resposta = file_get_contents($url);
+   
+    if ($resposta){ 
+        $dados = json_decode($resposta, true);
+
+    $nome = ucfirst($dados['name']);
+    $id = $dados['id'];
+    $imagem = $dados['sprites']['front_default'];
+    
+
+    echo "<h2>$nome (ID: $id)</h2>";
+    echo "<img src='$imagem' alt='$nome'><br>";
+} else {
+    echo "Pokemon nao encontrado";
+    }
+}
+?> 
